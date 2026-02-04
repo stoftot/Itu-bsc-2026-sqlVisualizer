4db802b2f3debe8a54c33f9e223b5f6a81bf0cf1
Mathias <math045b@gmail.com>
2026-02-04 17:54:30 +0100

    Added project structure

---
+++ sqlVisualizer/visualizer/Models/Table.cs
@@ -0,0 +1,11 @@
+﻿namespace visualizer.Models;
+
+public class Table
+{
+    public required List<List<string>> Entries { get; init; }
+
+    public List<string> GetColumnNames()
+    {
+        return Entries[0];
+    }
+}
