39eb233125cf8cc23147cfb65baea519a561eeea
Christoffer grünberg <gryn@itu.dk>
2026-02-04 19:00:48 +0100

    Create execute method in sql executor

--- sqlVisualizer/visualizer/Repositories/SQLExecutor.cs
+++ sqlVisualizer/visualizer/Repositories/SQLExecutor.cs
@@ -1,6 +1,32 @@
-﻿namespace visualizer.Repositories;
+﻿using Microsoft.Data.Sqlite;
+using visualizer.Models;
 
-public class SQLExecutor
+namespace visualizer.Repositories;
+
+public class SQLExecutor(SqliteConnection connection)
 {
-    
+    public async Task<Table> Execute(string sql)
+    {
+        var entries = new List<List<string>>();
+        await connection.OpenAsync();
+        await using var command = new SqliteCommand(sql, connection);
+        await using var reader = await command.ExecuteReaderAsync();
+        
+        var header = new List<string>();
+        for (var i = 0; i < reader.FieldCount; i++)
+        {
+            header.Add(reader.GetName(i));
+        }
+        
+        while (await reader.ReadAsync())
+        {
+            var row = new List<string>();
+            for (var i = 0; i < reader.FieldCount; i++)
+            {
+                row.Add(reader.GetValue(i).ToString() ?? "NULL");
+            }
+            entries.Add(row);
+        }
+        return new Table { Entries = entries.Prepend(header).ToList() };
+    }
 }
