b5b51aa672155dc36008516d93a15c719f067e98
Christoffer grünberg <gryn@itu.dk>
2026-02-04 18:17:00 +0100

    Added sql decomposition component model

---
+++ sqlVisualizer/visualizer/Models/SQLDecompositionComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace visualizer.Models;
+
+public class SQLDecompositionComponent(SQLKeyword keyword, string clause)
+{
+    public SQLKeyword Keyword { get; } = keyword;
+    public string Clause { get; } = clause;
+
+    public override string ToString()
+    {
+        return $"{Keyword.ToSQLString()} {Clause}";
+    }
+}
