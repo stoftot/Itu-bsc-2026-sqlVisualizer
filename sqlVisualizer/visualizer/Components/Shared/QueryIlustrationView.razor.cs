8e311fbc1b8a942454a5515dfe5e13dae3309934
Christoffer grünberg <gryn@itu.dk>
2026-02-06 10:48:34 +0100

    Create queryIlustration view component

---
+++ sqlVisualizer/visualizer/Components/Shared/QueryIlustrationView.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using visualizer.Models;
+
+namespace visualizer.Components.Shared;
+
+public class QueryIlustrationViewBase : ComponentBase
+{
+    [Parameter] public required List<Table> FromTables { get; init; }
+    [Parameter] public required Table ToTable { get; init; }
+}
