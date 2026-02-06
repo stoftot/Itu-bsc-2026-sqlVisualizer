1d81f418ab70c16d00b69b2e0d70e1bdcbfa4acb
Christoffer grünberg <gryn@itu.dk>
2026-02-06 19:10:58 +0100

    Updated illustratr view to take a querry and run against the database

--- sqlVisualizer/visualizer/Components/Shared/QueryIlustrationView.razor.cs
+++ sqlVisualizer/visualizer/Components/Shared/QueryIlustrationView.razor.cs
@@ -1,10 +1,77 @@
 ﻿using Microsoft.AspNetCore.Components;
 using visualizer.Models;
+using visualizer.Repositories;
 
 namespace visualizer.Components.Shared;
 
 public class QueryIlustrationViewBase : ComponentBase
 {
-    [Parameter] public required List<Table> FromTables { get; init; }
-    [Parameter] public required Table ToTable { get; init; }
+    [Parameter] public required string Query { get; init; }
+    [Inject] SQLExecutor SQLExecutor { get; init; }
+    public required List<Table> FromTables { get; init; } = [];
+    public required Table ToTable { get; set; }
+
+    private int _indexOfStepToHighlight = 0;
+
+    private int IndexOfStepToHighlight
+    {
+        get => _indexOfStepToHighlight;
+
+        set
+        {
+            if (value < 0 || value >= Steps.Count) return;
+            _indexOfStepToHighlight = value;
+            UpdateStepShown();
+        }
+    }
+
+    private List<SQLDecompositionComponent> Steps { get; set; }
+    private SQLDecompositionComponent IntialStep { get; set; }
+    private SQLDecomposer Decomposer { get; } = new();
+
+    protected override void OnInitialized()
+    {
+        Steps = Decomposer.Decompose(Query);
+        IntialStep = Steps.First();
+        Steps.Remove(IntialStep);
+
+        UpdateStepShown();
+        StateHasChanged();
+    }
+
+    private void UpdateStepShown()
+    {
+        FromTables.Clear();
+
+        if (IndexOfStepToHighlight == 0)
+        {
+            FromTables.Add(SQLExecutor.Execute(IntialStep).Result);
+        }
+        else
+        {
+            FromTables.Add(
+                SQLExecutor.Execute(
+                    Steps[..IndexOfStepToHighlight].Prepend(IntialStep)
+                ).Result);
+        }
+
+        var currentStep = Steps[IndexOfStepToHighlight];
+
+        if (currentStep.Keyword.IsJoin())
+        {
+            FromTables.Add(SQLExecutor.Execute(currentStep.GenerateFromClauseFromJoin()).Result);
+        }
+
+        ToTable = SQLExecutor.Execute(Steps[..(IndexOfStepToHighlight + 1)].Prepend(IntialStep)).Result;
+    }
+
+    protected void OnNextStep()
+    {
+        IndexOfStepToHighlight++;
+    }
+
+    protected void OnPreviousStep()
+    {
+        IndexOfStepToHighlight--;
+    }
 }
