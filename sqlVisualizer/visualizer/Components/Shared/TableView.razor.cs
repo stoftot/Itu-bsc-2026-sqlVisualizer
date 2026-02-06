e257b672b460efb16aa6d65651ffa3746caa354a
Christoffer grünberg <gryn@itu.dk>
2026-02-06 10:24:13 +0100

    Created table view component

---
+++ sqlVisualizer/visualizer/Components/Shared/TableView.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using visualizer.Models;
+
+namespace visualizer.Components.Shared;
+
+public class TableViewBase : ComponentBase
+{
+    [Parameter] public required Table Table { get; set; }
+}
