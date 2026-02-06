5942cdd1ffe67fbc5d620f2b7deb883806e23f44
Christoffer grünberg <gryn@itu.dk>
2026-02-06 10:44:39 +0100

    Created table ochestra component

---
+++ sqlVisualizer/visualizer/Components/Shared/TableOchestra.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using visualizer.Models;
+
+namespace visualizer.Components.Shared;
+
+public class TableOchestraBase : ComponentBase
+{
+    [Parameter] public required List<Table> Tables { get; init; }
+}
