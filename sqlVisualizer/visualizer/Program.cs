724008c95b8248a4bde6a3bae7c1c67f59634f1a
Christoffer grünberg <gryn@itu.dk>
2026-02-06 19:26:53 +0100

    made main work for mathias

--- sqlVisualizer/visualizer/Program.cs
+++ sqlVisualizer/visualizer/Program.cs
@@ -1,6 +1,7 @@
 using DuckDB.NET.Data;
 using visualizer;
 using visualizer.Components;
+using visualizer.Models;
 using visualizer.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,13 +27,37 @@ using var scope = app.Services.CreateScope();
 var conn = scope.ServiceProvider.GetRequiredService<DuckDBConnection>();
 
 var sqlExecutor = new SQLExecutor(conn);
-var query = "SELECT * FROM shift";
-var table = await sqlExecutor.Execute(query);
-Console.WriteLine($"Query: {query}");
-Console.WriteLine(string.Join(", ", table.ColumnNames));
-foreach (var row in table.Entries){
-    Console.WriteLine(string.Join(", ", row));
+// var query = "SELECT * FROM shift";
+// var table = await sqlExecutor.Execute(query);
+// Console.WriteLine($"Query: {query}");
+// Console.WriteLine(string.Join(", ", table.ColumnNames));
+// foreach (var row in table.Entries){
+//     Console.WriteLine(string.Join(", ", row));
+// }
+var query = "SELECT shift.day, user.email FROM shift " +
+            "JOIN \"user\" ON shift.cashier = \"user\".username";
+var components = new SQLDecomposer().Decompose(query);
+foreach (var component in components)
+{
+    Console.WriteLine($"Keyword: {component.Keyword}");
+    Console.WriteLine($"Clause: {component.Clause}");
+    Console.WriteLine("-----------------------------------------------------------------");
 }
+
+Console.WriteLine("\n\nExecuting components incrementally:\n");
+
+var currentComponets = new List<SQLDecompositionComponent>();
+foreach (var component in components)
+{
+    currentComponets.Add(component);
+    var result = await sqlExecutor.Execute(currentComponets);
+    Console.WriteLine($"Query: {component.ToExecutableClause()}");
+    Console.WriteLine(string.Join(", ", result.ColumnNames));
+    foreach (var row in result.Entries)
+        Console.WriteLine(string.Join(", ", row));
+    Console.WriteLine("-----------------------------------------------------------------");
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
