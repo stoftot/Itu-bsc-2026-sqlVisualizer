e921db49fa142975d8bf066511559e8ac6df1f66
Christoffer grünberg <gryn@itu.dk>
2026-02-06 22:40:28 +0100

    Added highligt style to table entry

--- sqlVisualizer/visualizer/Models/TableEntry.cs
+++ sqlVisualizer/visualizer/Models/TableEntry.cs
@@ -4,4 +4,22 @@ public class TableEntry
 {
     public bool IsHighlighted { get; set; } = false;
     public required List<string> Values { get; set; }
+    private string HighlightStyle { get; set; } = "";
+
+    public string GetHighlightStyle()
+    {
+        if (string.IsNullOrWhiteSpace(HighlightStyle))
+        {
+            SetHighlightStyleDefault();
+        }
+        return HighlightStyle;
+    }
+
+    public void SetHighlightStyleDefault() => HighlightStyle = "background-color: #FFF3CD;";
+
+    public void SetHighlightHexColor(string hexColor)
+    {
+        HighlightStyle = $"background-color: #{hexColor};";
+        Console.WriteLine(string.Join(",", Values));
+    }
 }
