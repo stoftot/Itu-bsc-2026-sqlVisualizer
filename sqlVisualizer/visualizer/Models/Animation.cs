2a7c6ea855d49eeef1dc9321a51813459d526f6f
Christoffer grünberg <gryn@itu.dk>
2026-02-11 09:50:51 +0100

    Created animation model

---
+++ sqlVisualizer/visualizer/Models/Animation.cs
@@ -0,0 +1,17 @@
+﻿namespace visualizer.Models;
+
+public class Animation(IReadOnlyList<Action> steps)
+{
+    private int currentStepIndex = 0;
+    private IReadOnlyList<Action> Steps { get; } = steps;
+
+    public bool NextStep()
+    {
+        if (currentStepIndex >= Steps.Count) return false;
+        Steps[currentStepIndex]();
+        currentStepIndex++;
+        return true;
+    }
+
+    public void Reset() => currentStepIndex = 0;
+}
