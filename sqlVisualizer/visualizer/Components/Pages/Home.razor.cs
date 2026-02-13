b8e82aee15350b03125f242fbfd3e5458928af0f
Mathias <math045b@gmail.com>
2026-02-13 14:58:16 +0100

    Refactored layout

---
+++ sqlVisualizer/visualizer/Components/Pages/Home.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using visualizer.Repositories;
+
+namespace visualizer.Components.Pages;
+
+
+public partial class Home : ComponentBase
+{
+    [Inject] public required State State { get; init; }
+    private string query = "";
+    
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        query = State.Queries[0].SQL;
+        /*State.RunSQL = sql =>
+        {
+            query = sql;
+            StateHasChanged();
+        }; */
+        Console.WriteLine("HERE ____________");
+        State.RunSQL = delegate(string sql)
+        {
+            query = sql;
+        };
+        Console.WriteLine("THERE ____________");
+    }
+}
