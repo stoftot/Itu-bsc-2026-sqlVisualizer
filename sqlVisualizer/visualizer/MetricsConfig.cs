a40a84e785424828d3a6cd23f0863d2e7e4baede
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2026-02-13 15:22:22 +0100

    Added button click counting to metrics.

--- sqlVisualizer/visualizer/MetricsConfig.cs
+++ sqlVisualizer/visualizer/MetricsConfig.cs
@@ -1,7 +1,15 @@
-﻿namespace visualizer;
+﻿using System.Diagnostics.Metrics;
+
+namespace visualizer;
 
 public class MetricsConfig
 {
     public const string ServiceName = "visualizer";
-    public const string ServiceVersion = "1.0.0"; 
+    public const string ServiceVersion = "1.0.0";
+
+    static readonly Meter Meter = new(ServiceName, ServiceVersion);
+
+    public readonly Counter<int> PrevButtonClicks = Meter.CreateCounter<int>("prev_clicks");
+    public readonly Counter<int> NextButtonClicks = Meter.CreateCounter<int>("next_clicks");
+    public readonly Counter<int> AnimateButtonClicks = Meter.CreateCounter<int>("animate_clicks");
 }
