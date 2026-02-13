4eef811ff2eb7e961abfd2637a134494c5e37dde
Christoffer grünberg <gryn@itu.dk>
2026-02-13 12:41:04 +0100

    Created visulisation model

---
+++ sqlVisualizer/visualizer/Models/Visualisation.cs
@@ -0,0 +1,9 @@
+﻿namespace visualizer.Models;
+
+public class Visualisation
+{
+    public Animation Animation { get; set; }
+    public List<Table> FromTables  { get; set; }
+    public Table ToTable { get; set; }
+    public SQLDecompositionComponent Component { get; set; }
+}
