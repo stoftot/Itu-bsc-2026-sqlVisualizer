0a84f3fff7445585824b08f95fe5704c384c247e
Christoffer grünberg <gryn@itu.dk>
2026-02-20 13:09:18 +0100

    Update Home.razor.cs

--- sqlVisualizer/visualizer/Components/Pages/Home.razor.cs
+++ sqlVisualizer/visualizer/Components/Pages/Home.razor.cs
@@ -10,18 +10,22 @@ public partial class Home : ComponentBase
     [Inject] public required State State { get; init; }
     private QueryIlustrationView QueryView; 
     private string query = "";
-    
-    
-    protected override void OnInitialized()
+
+    protected override void OnAfterRender(bool firstRender)
     {
-        base.OnInitialized();
-        query = State.Queries[0].SQL;
+        if (!firstRender) return;
+        Console.WriteLine("did an intialize");
         State.RunSQL = sql =>
         {
             query = sql;
             StateHasChanged();
             QueryView.Init();
         };
+    }
+
+    protected override void OnInitialized()
+    {
+        query = State.Queries[0].SQL;
         State.SessionId = Http.HttpContext?.Request.Cookies["session_id"] ?? "unknown";
     }
 }
