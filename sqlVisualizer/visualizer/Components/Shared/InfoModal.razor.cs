f6584f3433c89f55c04f70f3004a23f57cd1dad9
Mathias <math045b@gmail.com>
2026-02-20 15:32:46 +0100

    info-modal

---
+++ sqlVisualizer/visualizer/Components/Shared/InfoModal.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace visualizer.Components.Shared;
+
+public partial class InfoModal : ComponentBase
+{
+    public required bool ShowModal = true;
+    public void ToggleModal()
+    {
+        ShowModal = !ShowModal;
+        StateHasChanged();
+    }
+}
