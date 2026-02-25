e4d70c59c14f6bedd4171d19b34e6c2a9cbc8c7f
Christoffer grünberg <gryn@itu.dk>
2026-02-25 13:12:05 +0100

    Created interface an dummy implementation

---
+++ sqlVisualizer/visualizer/Repositories/DummyMetricsHandler.cs
@@ -0,0 +1,30 @@
+using visualizer.Models;
+
+namespace visualizer.Repositories;
+
+public class DummyMetricsHandler : IMetricsHandler
+{
+    public void IncrementAction(string sessionId, ActionType actionType)
+    { }
+
+    public void RecordQuery(string sessionId, string query)
+    { }
+
+    public void PrintQueries(string sessionId)
+    { }
+
+    public void PrintActions(string sessionId)
+    { }
+
+    public void EnterStep(string sessionId, SQLKeyword step)
+    { }
+
+    public void StartAnimation(string sessionId)
+    { }
+
+    public void StopAnimation(string sessionId)
+    { }
+
+    public void PrintSessionTimings(string sessionId)
+    { }
+}
