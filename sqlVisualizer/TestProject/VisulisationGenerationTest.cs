02cb53e68a4f0efc5ba65a75fc20e95de270ab48
Christoffer grünberg <gryn@itu.dk>
2026-02-25 12:39:52 +0100

    Created database fixture, and first test

--- sqlVisualizer/TestProject/VisulisationGenerationTest.cs
+++ sqlVisualizer/TestProject/VisulisationGenerationTest.cs
@@ -1,17 +1,34 @@
+using DuckDB.NET.Data;
 using visualizer.Repositories;
 
 namespace TestProject1;
 
-public class VisulisationGenerationTest
+[Collection("DuckDb seeded")]
+public class VisulisationGenerationTest : IClassFixture<DuckDbFixture>
 {
+    private readonly VisualisationsGenerator generator;
+    public VisulisationGenerationTest(DuckDbFixture fixture)
+    {
+        generator = new VisualisationsGenerator(
+            new SQLDecomposer(), 
+            new TableGenerator(new SQLExecutor(fixture.CreateConnection())),  
+            new TableOriginColumnsGenerator());
+    }
+    
     [Fact]
     public void Join()
     {
-        var query = "";
+        const string query =
+            """
+            SELECT * FROM product
+            JOIN purchase ON product.productname = purchase.productname
+            """;
+        TestQuery(query);
     }
 
     private void TestQuery(string query)
     {
-        
+        var visualisations = generator.Generate(query);
+        Assert.True(visualisations.Count > 0);
     }
 }
