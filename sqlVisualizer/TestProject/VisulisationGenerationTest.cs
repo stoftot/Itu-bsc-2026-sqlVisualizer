174c5728e1d22fc21a5e5c119e24cbf975911a17
Christoffer grünberg <gryn@itu.dk>
2026-03-03 13:23:02 +0100

    Impented some simpel tests

--- sqlVisualizer/TestProject/VisulisationGenerationTest.cs
+++ sqlVisualizer/TestProject/VisulisationGenerationTest.cs
@@ -7,22 +7,103 @@ namespace TestProject1;
 public class VisulisationGenerationTest : IClassFixture<DuckDbFixture>
 {
     private readonly VisualisationsGenerator generator;
+
     public VisulisationGenerationTest(DuckDbFixture fixture)
     {
         generator = new VisualisationsGenerator(
-            new SQLDecomposer(), 
-            new TableGenerator(new SQLExecutor(fixture.CreateConnection())),  
+            new SQLDecomposer(),
+            new TableGenerator(new SQLExecutor(fixture.CreateConnection()), new TableOriginColumnsGenerator()),
             new TableOriginColumnsGenerator());
     }
-    
-    [Fact]
-    public void Join()
+
+    // [Theory]
+    // [InlineData("""
+    //             
+    //             """)]
+    // public void Template(string query)
+    // {
+    //     TestQuery(query);
+    // }
+
+    [Theory]
+    [InlineData("""
+                SELECT shift.day FROM shift
+                """)]
+    [InlineData("""
+                SELECT day FROM shift
+                """)]
+    [InlineData("""
+                SELECT * FROM shift
+                """)]
+    public void Select(string query)
     {
-        const string query =
-            """
-            SELECT * FROM product
-            JOIN purchase ON product.productname = purchase.productname
-            """;
+        TestQuery(query);
+    }
+
+    [Theory]
+    [InlineData("""
+                SELECT productname, username FROM purchase
+                where username == 'Martin'
+                """)]
+    [InlineData("""
+                SELECT productname, username FROM purchase
+                where username LIKE 'M%'
+                """)]
+    public void Where(string query)
+    {
+        TestQuery(query);
+    }
+
+    [Theory]
+    [InlineData("""
+                SELECT * FROM product
+                JOIN purchase ON product.productname = purchase.productname
+                """)]
+    [InlineData("""
+                SELECT product.productname, purchase.purchasetime, user.username FROM product
+                JOIN purchase ON product.productname = purchase.productname
+                JOIN user on purchase.username = user.username
+                """)]
+    public void Join(string query)
+    {
+        TestQuery(query);
+    }
+
+    [Theory]
+    [InlineData("""
+                SELECT productname FROM purchase 
+                GROUP BY productname
+                """)]
+    [InlineData("""
+                SELECT productname, username FROM purchase 
+                GROUP BY productname, purchase.username
+                """)]
+    public void GroupBy(string query)
+    {
+        TestQuery(query);
+    }
+
+    [Theory]
+    [InlineData("""
+                SELECT coUnT() FROM purchase
+                """)]
+    public void AggreGateFunctions(string query)
+    {
+        TestQuery(query);
+    }
+
+    [Theory]
+    [InlineData("""
+                SELECT productname, COUNT() FROM purchase 
+                GROUP BY productname
+                """)]
+    [InlineData("""
+                SELECT productname, user.email, COUNT() FROM purchase
+                JOIN user ON user.username = purchase.username
+                GROUP BY productname, user.email
+                """)]
+    public void Combination(string query)
+    {
         TestQuery(query);
     }
 
