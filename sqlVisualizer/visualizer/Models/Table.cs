ef658f5fbd1b047207cf0bdd6aa42240d74bbd30
Christoffer grünberg <gryn@itu.dk>
2026-03-06 16:32:22 +0100

    Updated tables to allow for alias of columnNames

--- sqlVisualizer/visualizer/Models/Table.cs
+++ sqlVisualizer/visualizer/Models/Table.cs
@@ -7,14 +7,17 @@ public class Table
     /// otherwise it is considered a result table and has an empty string as name.
     /// </summary>
     public string Name { get; set; } = string.Empty;
+
     public bool IsResultTable => Name == string.Empty;
+
     /// <summary>
     /// The names of the tables the columns originally comes from so things like table.columnName
     /// can be handled.
     /// If a name is "()" it means it's an aggregate function and it therefore dost have an original table
     /// </summary>
     public List<string> ColumnsOriginalTableNames { get; private init; } = [];
-    public required IReadOnlyList<string> ColumnNames { get; init; }
+
+    public required List<string> ColumnNames { get; init; }
     public required IReadOnlyList<TableEntry> Entries { get; init; }
 
     public Table DeepClone()
@@ -46,7 +49,7 @@ public class Table
                 return i;
             }
         }
-        
+
         throw new ArgumentException($"Column {column} not found in table {tableName}");
     }
 }
