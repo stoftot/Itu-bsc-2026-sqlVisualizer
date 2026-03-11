40e7ee2abe7d7db13b927ad89d682ad771cbb0e9
Mathias <math045b@gmail.com>
2026-03-11 13:34:16 +0100

    Removed Backgrond colors
    Because we can't use them in css

--- sqlVisualizer/visualizer/Utility/UtilColor.cs
+++ sqlVisualizer/visualizer/Utility/UtilColor.cs
@@ -2,10 +2,6 @@ namespace visualizer.Utility;
 
 public static class UtilColor
 {
-    // Background
-    public static readonly string ToolbarBackgroundColor = "#E5E7EB";
-    public static readonly string SchemaBackgroundColor = "#F1F5F9";
-    
     // Highlighting
     public static readonly string PrimaryHighlightColor = "#FFF3CD";
     public static readonly string SecondaryHiglightColor = "#cdebff";
