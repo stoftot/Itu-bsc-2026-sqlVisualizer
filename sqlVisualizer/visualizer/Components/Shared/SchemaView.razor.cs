830fd63d3c685c6d3d10985c9993f7c23f7a2da4
Christoffer grünberg <gryn@itu.dk>
2026-03-20 18:55:43 +0100

    Updated how table names and there columns is displayed

--- sqlVisualizer/visualizer/Components/Shared/SchemaView.razor.cs
+++ sqlVisualizer/visualizer/Components/Shared/SchemaView.razor.cs
@@ -8,9 +8,17 @@ public partial class SchemaView : ComponentBase
 {
     [Inject] SQLExecutor SQLExecutor { get; init; }
     public required Database Database { get; set; }
+    [Parameter]
+    public EventCallback<Table> OnTableSelected { get; set; }
+
     protected override void OnInitialized()
     {
         Database = SQLExecutor.GetDatabase().Result;
         StateHasChanged();
+    }
+
+    private async Task HandleTableClick(Table table)
+    {
+        await OnTableSelected.InvokeAsync(table);
     }
 }
