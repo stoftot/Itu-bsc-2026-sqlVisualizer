9f5b55b86c4afae95e86e4fc8cfd297b5bcc9fba
Christoffer grünberg <gryn@itu.dk>
2026-04-06 19:57:18 +0200

    created editor

---
+++ sqlVisualizer/visualizer/Components/Pages/SqlEditor.razor.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Components;
+using BlazorMonaco.Editor;
+using visualizer.Models;
+using visualizer.Repositories;
+
+namespace visualizer.Components.Pages;
+
+public partial class SqlEditor : ComponentBase
+{
+    [Inject] public required SQLExecutor SQLExecutor { get; init; }
+
+    protected StandaloneCodeEditor Editor = null!;
+    protected Table? tableSelected;
+    protected Table? resultTable;
+    protected bool viewResult = true;
+    protected bool HasExecutionError;
+    protected string ExecutionErrorMessage = string.Empty;
+    
+    private string _currentEditorQuery = string.Empty;
+
+    protected override void OnInitialized()
+    {
+    }
+
+    private StandaloneEditorConstructionOptions EditorConstructionOptions(StandaloneCodeEditor editor)
+    {
+        return new StandaloneEditorConstructionOptions
+        {
+            AutomaticLayout = true,
+            Language = "sql",
+            Value = _currentEditorQuery,
+            Minimap = new EditorMinimapOptions { Enabled = false }
+        };
+    }
+
+    private async Task OnDidChangeModelContent(ModelContentChangedEvent _)
+    {
+        _currentEditorQuery = await Editor.GetValue() ?? string.Empty;
+    }
+
+    private async Task RunSqlAsync(string sql)
+    {
+        try
+        {
+            HasExecutionError = false;
+            ExecutionErrorMessage = string.Empty;
+            resultTable = await SQLExecutor.Execute(sql);
+        }
+        catch (Exception e)
+        {
+            HasExecutionError = true;
+            ExecutionErrorMessage = e.Message;
+            resultTable = null;
+        }
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    protected void HandTabledSchemaTableSelected(Table table)
+    {
+        viewResult = false;
+        tableSelected = table;
+        StateHasChanged();
+    }
+
+    private async Task RunQuery()
+    {
+        var editorContent = await Editor.GetValue() ?? "";
+        _currentEditorQuery = editorContent;
+        await RunSqlAsync(editorContent);
+        viewResult = true;
+        StateHasChanged();
+    }
+
+    private void CloseErrorPopup()
+    {
+        HasExecutionError = false;
+        ExecutionErrorMessage = string.Empty;
+        StateHasChanged();
+    }
+}
