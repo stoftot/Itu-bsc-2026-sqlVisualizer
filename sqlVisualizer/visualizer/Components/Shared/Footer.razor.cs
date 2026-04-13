5065f1d1e52112e9f13695023750286ebf0282b7
Mathias <math045b@gmail.com>
2026-04-13 09:18:12 +0200

    Copy session id

--- sqlVisualizer/visualizer/Components/Shared/Footer.razor.cs
+++ sqlVisualizer/visualizer/Components/Shared/Footer.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using visualizer.Repositories;
 
 namespace visualizer.Components.Shared;
@@ -6,6 +7,13 @@ namespace visualizer.Components.Shared;
 public partial class Footer : ComponentBase
 {
     [Inject] HomeState HomeState { get; set; }
+    [Inject] IJSRuntime JS { get; set; }
     
-    
+    private async Task CopySessionIdToClipboard()
+    {
+        if (HomeState?.SessionId != null)
+        {
+            await JS.InvokeVoidAsync("navigator.clipboard.writeText", HomeState.SessionId);
+        }
+    }
 }
