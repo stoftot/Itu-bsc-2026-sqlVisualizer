1b684a2b7e661b161f28e58aeab409a6a298e749
Christoffer grünberg <gryn@itu.dk>
2026-04-15 11:43:31 +0200

    Added check to visulisation generator to make sure we dont generate entries with to many columns

--- sqlVisualizer/visualizer/Repositories/VisualisationsGenerator.cs
+++ sqlVisualizer/visualizer/Repositories/VisualisationsGenerator.cs
@@ -46,6 +46,7 @@ public class VisualisationsGenerator(ISQLDecomposer decomposer, TableGenerator t
             tg.GenerateFromTablesWithOriginColumns(currStep, fromTables, prevToTables, currSteps);
             
             ValidateOriginColumnsCount(fromTables, currStep);
+            ValidateThatNumberColumnsInEntriesMatchWithNumberOfColumnsInTables(fromTables, currStep);
             
             //Generate to tables
             var currVis = tg.GenerateToTable(currStep, currSteps, fromTables, toTables);
@@ -57,6 +58,7 @@ public class VisualisationsGenerator(ISQLDecomposer decomposer, TableGenerator t
             //Generate origin on to tables
             tocg.GenerateTableOriginOnToTablesColumns(currVis);
             ValidateOriginColumnsCount(currVis.ToTables, currVis.Component);
+            ValidateThatNumberColumnsInEntriesMatchWithNumberOfColumnsInTables(currVis.ToTables, currVis.Component);
             
             visualisations.Add(currVis);
         }
@@ -80,6 +82,19 @@ public class VisualisationsGenerator(ISQLDecomposer decomposer, TableGenerator t
             if (table.ColumnsOriginalTableNames.Count != table.ColumnNames.Count)
                 throw new Exception("count of original table names are supposed to match with the count of columns" +
                                     $"\n{table.ColumnsOriginalTableNames.Count} :  {table.ColumnNames.Count}"+
+                                    $"\nStatment: \"{component}\"");
+        }
+    }
+
+    private void ValidateThatNumberColumnsInEntriesMatchWithNumberOfColumnsInTables
+        (List<Table> tables, SQLDecompositionComponent component)
+    {
+        foreach (var table in tables)
+        {
+            var numberOfColumnsInTables = table.ColumnNames.Count;
+            var correct = table.Entries.TrueForAll(e => e.Values.Count == numberOfColumnsInTables);
+            if (!correct)
+                throw new Exception("table contains incorrect number of entries" +
                                     $"\nStatment: \"{component}\"");
         }
     }
